9597e08f96fc6c8b73b0872c51a402212f0d8d11
Jose Roa <jg.roac@gmail.com>
2020-09-26 02:29:50 +0100

    add personal tweaks

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 # Welcome!!
 # Custom dotfiles to get you started with OS X machine for development.
-# Author: https://github.com/gokulkrishh
-# Source: https://github.com/gokulkrishh/dotfiles
+# Authors:
+# https://github.com/gokulkrishh
+# https://github.com/jgroac
+# Source: https://github.com/jgroac/dotfiles
 
 ## Custom color codes & utility functions
 source helper/utility.sh
@@ -22,7 +24,6 @@ e_bold "${tan}┌─────────────────────
 |                                                              |
 | Setup your OS X machine for web development at ease.         |
 |                                                              |
-| Author: https://github.com/gokulkrishh                       |
 |                                                              |
 └──────────────────────────────────────────────────────────────┘"
 
@@ -67,7 +68,9 @@ else
   cp oh-my-zsh/zshrc ~/.zshrc                                            ## Copy zshrc configs
   cp oh-my-zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme   ## Copy custom dracula theme
   cp oh-my-zsh/z.sh ~/z.sh                                               ## Copy z.sh autocompletion file
-  git clone https://github.com/peterhurford/git-it-on.zsh ~/.oh-my-zsh/custom/plugins/git-it-on ## Copy git it on utilities plugin
+  git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k ## zsh theme
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ## Copy syntax higlighting pluging
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ## Copy autosuggestions pluging
 fi
 
 ## Create codelabs & workspace directory
@@ -114,6 +117,66 @@ fi
 echo "node --version: $(node --version)"
 echo "npm --version: $(npm --version)"
 
+
+## My touchbar. My rules
+if ! type mtmr > /dev/null
+then
+  e_header "Install My touchbar. My rules.."
+  brew cask install mtmr
+  cp -rf osx/items.json ~/Library/Application\ Support/MTMR/items.json
+fi
+
+## Serve static files
+if ! type serve > /dev/null
+then
+  e_header "Install serve.."
+  yarn global add serve
+fi
+
+## thefuck install
+if ! type fuck > /dev/null
+then
+  e_header "Install thefuck.."
+  brew install thefuck
+fi
+
+
+## Bat install
+if ! type bat > /dev/null
+then
+  e_header "Install bat.."
+  brew install bat
+
+  e_header "Install bat themes.."
+  mkdir -p "$(bat --config-dir)/themes"
+  cd "$(bat --config-dir)/themes"
+
+  # Download a theme in '.tmTheme' format, for example:
+  git clone https://github.com/batpigandme/night-owlish
+
+  # Update the binary cache
+  bat cache --build
+fi
+
+
+## Ripgrep install
+if ! type ripgrep > /dev/null
+then
+  e_header "Install ripgrep.."
+  brew install ripgrep
+fi
+
+
+## git-delta install
+if ! type delta > /dev/null
+then
+  e_header "Install git-delta.."
+  brew install git-delta
+  git config --global core.pager="delta --plus-color=\"#012800\" --minus-color=\"#340001\" --theme='night-owlish'"
+  git config --global interactive.diffFilter="delta --color-only"
+fi
+
+
 echo "Generating an RSA token for GitHub"
 ssh-keygen -t rsa -b 4096 -C "jg.roac@gmail.com"
 echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
@@ -124,8 +187,6 @@ echo "run 'pbcopy < ~/.ssh/id_rsa.pub' and paste that into GitHub"
 if [ -d ~/dotfiles ]; then
   sudo rm -R ~/dotfiles
 fi
-
-e_thanks "Author: https://github.com/gokulkrishh \n"
 
 echo "🍺  Thats all, Done. Note that some of these changes require a logout/restart to take effect."
 
